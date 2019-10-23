1ce2afbb700f46067834c824227eaf1c367e29c9
BearsNBeets <kobo67@hotmail.com>
2019-10-23 16:35:00 +1100

    Changed start up effect
    Sound effect and random voice message plays on start up

--- start.py
+++ start.py
@@ -1,14 +1,23 @@
+from os import path
 import sys
+import random
 sys.path.append('scripts')
 
-from audioReply import playAudio
+from audioReply import playAudio, getAudioReply
 from backgroundListener import run_background_assistant
+import profileManager
+import settings
 
 def startAssistant():
 	print("Starting assistant")
-	playAudio("res\\audio\\system-start-up.mp3")
+	startup_audio()
+	profileManager.init()
 	run_background_assistant()
 
+def startup_audio():
+	playAudio(path.join(settings.AUDIO_DIR, "system-start-up.mp3"))
+	random_clip = str(random.randint(0,3))
+	playAudio(path.join(settings.AUDIO_DIR, "system-start-up-voice" + random_clip + ".mp3"))
 
 if __name__ == '__main__':
 	startAssistant()
