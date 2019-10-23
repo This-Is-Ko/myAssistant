11cd01df233ea7af9014176a5945e98ede06d4ef
BearsNBeets <kobo67@hotmail.com>
2019-10-23 13:30:14 +1100

    Improved Google search function
    Prevent window from closing directly after task has been complete

--- googleSearch.py
+++ googleSearch.py
@@ -1,19 +1,31 @@
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options
+
 
 def search_google(search_text):
-	# Start Chrome browser
-	browser = driver = webdriver.Chrome("C:\\bin\\chromedriver.exe")
-	# Go to Google search page
-	browser.get('https://www.google.com')
-	# Find search field and search text
-	search = browser.find_element_by_name('q')
-	search.send_keys(search_text)
-	search.submit()
+	print("Googling... " + search_text)
+	try:
+		# Start Chrome browser
+		chrome_options = Options()
+		chrome_options.add_experimental_option("detach", True)
+		browser = driver = webdriver.Chrome("C:\\bin\\chromedriver.exe", options = chrome_options)
+		# Go to Google search page
+		browser.get('https://www.google.com')
+		# Find search field and search text
+		search = browser.find_element_by_name('q')
+		search.send_keys(search_text)
+		search.submit()
+	except TimeoutException:
+		print(TimeoutException)
+	except:
+		print("Unknown error. Try again later")
+
 
 def main(search_text):
 	search_google(search_text)
 
 
 if __name__ == '__main__':
-	main("search text")
+	main("python")
+	
