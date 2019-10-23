159b23bf2ea6b1332b42f609e0e81d3be2fe018b
BearsNBeets <kobo67@hotmail.com>
2019-10-23 13:33:49 +1100

    Fixed start.py
    Play start-up sound before running assistant

--- start.py
+++ start.py
@@ -1,6 +1,12 @@
-from alwaysListeningBackgroundAudio import recordAudio
-def main():
-	recordAudio()
+from audioReply import playAudio
+from backgroundListener import run_background_assistant
+
+def startAssistant():
+	print("Starting assistant")
+	playAudio("res\\audio\\system-start-up.mp3")
+	run_background_assistant()
+
 
 if __name__ == '__main__':
-	main()
+	startAssistant()
+	
